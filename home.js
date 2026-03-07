5e965086669b3fbea24de9ac5a9b1ac853586202
NafizImrose <nafizimrose.official@gmail.com>
2026-03-08 00:32:06 +0600

    Add button functionality and active state styling; implement tab switching in home.js and style.css

--- home.js
+++ home.js
@@ -1,1 +1,23 @@
-console.log("running");
+const tabBtn = (id, btn) => {
+  const all = document.getElementById("all");
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+
+  all.classList.add("hidden");
+  open.classList.add("hidden");
+  close.classList.add("hidden");
+
+  const select = document.getElementById(id);
+  select.classList.remove("hidden");
+  // *********************************
+  const allBtn = document.getElementById("all-btn");
+  const openBtn = document.getElementById("open-btn");
+  const closeBtn = document.getElementById("close-btn");
+
+  allBtn.classList.remove("active");
+  openBtn.classList.remove("active");
+  closeBtn.classList.remove("active");
+
+  const activeBtn = document.getElementById(btn);
+  activeBtn.classList.add("active");
+};
