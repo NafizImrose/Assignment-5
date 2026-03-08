754f3042d2605c6a9cdf030dc090f6a25a98390b
NafizImrose <nafizimrose.official@gmail.com>
2026-03-08 22:12:37 +0600

    card logo done

--- home.js
+++ home.js
@@ -57,15 +57,13 @@ let openCnt = 0;
 let closeCnt = 0;
 const allissues = (issues) => {
   issues.forEach((issue) => {
-    if (issue.status === "open") {
-      openCnt++;
-      const newDiv = document.createElement("div");
-      newDiv.innerHTML = `
-        <div
-          class="card p-6 bg-white shadow-md border-t-5 border-[#00A96E] rounded-md"
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+        <div id="card-${issue.id}"
+          class="card p-6 bg-white shadow-md border-t-5  rounded-md"
         >
           <div class="flex justify-between">
-            <div><img src="assets/Open-Status.png" alt="" /></div>
+            <div id="logo-${issue.id}"></div>
             <div id="badge_${issue.id}" class="px-5 py-1  rounded-full">
               <h1 id="badge-text-${issue.id}" class=" text-sm">${issue.priority}</h1>
             </div>
@@ -94,68 +92,47 @@ const allissues = (issues) => {
         </div>
         `;
 
-      const allSec = document.getElementById("all");
+    const allSec = document.getElementById("all");
+
+    allSec.append(newDiv);
+
+    // badge background color setting
+    const badge = document.getElementById(`badge_${issue.id}`);
+    const badgeText = document.getElementById(`badge-text-${issue.id}`);
+    if (issue.priority === "high") {
+      badge.classList.add("bg-[#FEECEC]");
+      badgeText.classList.add("text-[#EF4444]");
+    } else if (issue.priority === "medium") {
+      badge.classList.add("bg-[#FFF6D1]");
+      badgeText.classList.add("text-[#F59E0B]");
+    } else if (issue.priority === "low") {
+      badge.classList.add("bg-[#EEEFF2]");
+      badgeText.classList.add("text-[#9CA3AF]");
+    }
+    //   card border and logo
+    const borderColor = document.getElementById(`card-${issue.id}`);
+
+    //card logo
+    const openLogo = document.createElement("div");
+    openLogo.innerHTML = `<img src="assets/Open-Status.png" alt="" />`;
+    const closeLogo = document.createElement("div");
+    closeLogo.innerHTML = `<img src="assets/Closed- Status .png" alt="" />`;
+
+    const logo = document.getElementById(`logo-${issue.id}`);
+
+    if (issue.status === "open") {
+      openCnt++;
+      logo.append(openLogo);
+      borderColor.classList.add("border-[#00A96E]");
       const openSec = document.getElementById("open");
-      allSec.append(newDiv);
-
-      // badge background color setting
-      const badge = document.getElementById(`badge_${issue.id}`);
-      const badgeText = document.getElementById(`badge-text-${issue.id}`);
-      if (issue.priority === "high") {
-        badge.classList.add("bg-[#FEECEC]");
-        badgeText.classList.add("text-[#EF4444]");
-      } else if (issue.priority === "medium") {
-        badge.classList.add("bg-[#FFF6D1]");
-        badgeText.classList.add("text-[#F59E0B]");
-      } else if (issue.priority === "low") {
-        badge.classList.add("bg-[#EEEFF2]");
-        badgeText.classList.add("text-[#9CA3AF]");
-      }
-      //   ***********************************
-
       const clone = newDiv.cloneNode(true);
       openSec.append(clone);
     } else if (issue.status === "closed") {
       closeCnt++;
-      const newDiv2 = document.createElement("div");
-      newDiv2.innerHTML = `
-        <div
-          class="card p-6 bg-white shadow-md border-t-5 border-[#A855F7] rounded-md"
-        >
-          <div class="flex justify-between">
-            <div><img src="assets/Closed- Status .png" alt="" /></div>
-            <div class="px-5 py-1 bg-[#FEECEC] rounded-full">
-              <h1 class="text-red-500 text-sm">HIGH</h1>
-            </div>
-          </div>
-          <h1 class="font-bold mt-3 mb-2 text-sm">
-            Fix navigation menu on mobile devices
-          </h1>
-          <p class="text-sm pb-3 text-[#64748B]">
-            The navigation menu doesn't collapse properly on mobile devices...
-          </p>
-
-          <div class="mother-bug flex gap-2 mb-4">
-            <div
-              class="px-2 py-1 bg-[#FEECEC] border border-red-600 rounded-full"
-            >
-              <h1 class="text-red-500 text-sm">
-                <i class="fa-solid fa-bug"></i> BUG
-              </h1>
-            </div>
-          </div>
-          <div class="-mx-6 border-t border-gray-300"></div>
-          <div class="time py-3">
-            <p class="text-[#64748B]">#1 by john_doe</p>
-            <p class="text-[#64748B]">1/15/2024</p>
-          </div>
-        </div>
-        `;
-      const allSec = document.getElementById("all");
+      logo.append(closeLogo);
+      borderColor.classList.add("border-[#A855F7]");
       const closeSec = document.getElementById("close");
-      allSec.append(newDiv2);
-
-      const clone = newDiv2.cloneNode(true);
+      const clone = newDiv.cloneNode(true);
       closeSec.append(clone);
     }
   });
